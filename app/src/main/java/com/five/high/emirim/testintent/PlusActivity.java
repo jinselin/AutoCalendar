65ee7103d470520b9beec3d61eb455ccc50d94c7
Selin jin <s2017i17@e-mirim.hs.kr>
2017-09-18 20:46:43 +0900

    #6 이벤트 추가 화면 추가

--- app/src/main/java/com/five/high/emirim/testintent/PlusActivity.java
+++ app/src/main/java/com/five/high/emirim/testintent/PlusActivity.java
@@ -1,13 +1,23 @@
 package com.five.high.emirim.testintent;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 public class PlusActivity extends AppCompatActivity {
+
+    private static final String TAG ="오캘:PlusActivity";
+
+    public PlusActivity(Intent intent) {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plus);
+        Button buttonA;
+        Button buttonB;
+
     }
 }
